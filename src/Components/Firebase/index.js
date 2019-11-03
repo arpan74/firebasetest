1fd33c5b1b0b52fb62f6ef067e975ff0dac5702f
Arpan Gupta <arpan74@gmail.com>
2019-11-02 20:26:29 -0400

    cleaned up firebase file

--- src/Components/Firebase/index.js
+++ src/Components/Firebase/index.js
@@ -14,28 +14,12 @@ const firebaseConfig = {
 
 class Firebase {
   constructor() {
-    app.initializeApp(firebaseConfig);
+    console.log(app);
+    // app.initializeApp(firebaseConfig);
     console.log(app);
 
     this.auth = app.auth();
   }
-
-  // *** Auth API ***
-  doCreateUserWithEmailAndPassword = (email, password) => {
-    return this.auth.doCreateUserWithEmailAndPassword(email, password);
-  };
-
-  doSignInWithEmailAndPassword = (email, password) => {
-    return this.auth.doSignInWithEmailAndPassword(email, password);
-  };
-
-  doSignOut = () => this.auth.signOut();
-
-  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-  doPasswordUpdate = password => {
-    this.auth.currentUser.updatePassword(password);
-  };
 }
 
 export default Firebase;
